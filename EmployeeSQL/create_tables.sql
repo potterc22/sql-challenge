3446110a46de37b43da18b130fb9da6b854f7566
Cory Potter <pottercory22@gmail.com>
2020-09-30 14:48:11 -0500

    Fixed Primary/Foreign Keys

--- EmployeeSQL/create_tables.sql
+++ EmployeeSQL/create_tables.sql
@@ -1,35 +1,42 @@
-CREATE TABLE dept_emp (
-	emp_no int NOT NULL,
-	dept_no varchar(4) NOT NULL
-);
-
 CREATE TABLE departments (
 	dept_no varchar(4) PRIMARY KEY,
 	dept_name varchar NOT NULL
-);
-
-CREATE TABLE dept_manager (
-	dept_no varchar(4) NOT NULL,
-	emp_no int NOT NULL
-);
-
-CREATE TABLE employees (
-	emp_no int PRIMARY KEY,
-	emp_title_id VARCHAR(5) NOT NULL,
-	birth_date DATE NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	sex VARCHAR(1) NOT NULL,
-	hire_date DATE NOT NULL
-);
-
-
-CREATE TABLE salaries (
-	emp_no int PRIMARY KEY,
-	salary int NOT NULL
 );
 
 CREATE TABLE titles (
 	title_id VARCHAR(5) PRIMARY KEY,
 	title VARCHAR NOT NULL
 );
+
+CREATE TABLE employees (
+	emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR(5) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+DROP TABLE dept_emp
+CREATE TABLE dept_emp (
+	emp_no int NOT NULL,
+	dept_no varchar(4) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE dept_manager (
+	dept_no varchar(4) NOT NULL,
+	emp_no int PRIMARY KEY NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE salaries (
+	emp_no int PRIMARY KEY,
+	salary int NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
