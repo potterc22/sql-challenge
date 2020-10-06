ea47b9953d6fb59e3fc91063b78426d1398372d6
Cory Potter <pottercory22@gmail.com>
2020-10-05 20:14:45 -0500

    Small updates to Query 2 and 3

--- EmployeeSQL/query2.sql
+++ EmployeeSQL/query2.sql
@@ -4,5 +4,5 @@ SELECT first_name,
 	last_name,
 	hire_date
 FROM employees
-WHERE hire_date >= '1/1/1986'
-	OR hire_date <= '12/31/1986'
+WHERE hire_date >= '1986-01-01'
+	AND hire_date <= '1986-12-31'
--- EmployeeSQL/query3.sql
+++ EmployeeSQL/query3.sql
@@ -8,5 +8,5 @@ SELECT dept_manager.dept_no,
 	employees.last_name,
 	employees.first_name
 FROM dept_manager
-INNER JOIN departments ON dept_manager.dept_no = departments.dept_no
-INNER JOIN employees ON dept_manager.emp_no = employees.emp_no
+LEFT JOIN departments ON dept_manager.dept_no = departments.dept_no
+LEFT JOIN employees ON dept_manager.emp_no = employees.emp_no
